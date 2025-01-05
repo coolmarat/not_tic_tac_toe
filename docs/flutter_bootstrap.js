3d4836733c774274e73f84fc96b69d2de2dd5527
marov <marov@stsoft.ru>
2025-01-05 09:42:55 +0300

    index edited

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "864688901"
+    serviceWorkerVersion: "1200457417"
   }
 });
